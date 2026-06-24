397f10c4423f2956e3fcb6aa7becba2bda6f94e4
karthik449213 <karthikpiinasi@gmail.com>
2026-06-24 06:28:19 +0000

    feat : create animated battery guage widget

---
+++ voltwatch/lib/presentation/widgets/battery_guage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BatteryGauge extends StatelessWidget {
+  final int percentage;
+
+  const BatteryGauge({
+    super.key,
+    required this.percentage,
+  });
+
+  Color getColor() {
+    if (percentage >= 80) return Colors.green;
+    if (percentage >= 40) return Colors.amber;
+    return Colors.red;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 180,
+      height: 180,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          TweenAnimationBuilder<double>(
+            tween: Tween(
+              begin: 0,
+              end: percentage / 100,
+            ),
+            duration: const Duration(milliseconds: 700),
+            builder: (context, value, child) {
+              return CircularProgressIndicator(
+                value: value,
+                strokeWidth: 12,
+              );
+            },
+          ),
+          Text(
+            "$percentage%",
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
